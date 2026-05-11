43657b51dda45318dc87d92667a6186b03264e5b
mohammad2745 <Mohammad.Sultan@mindstaq.com>
2026-05-11 19:46:32 +0600

    Initial setup

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const signin = () => {
+  return (
+    <div>
+      <h1>Sign In</h1>
+    </div>
+  );
+};
+
+export default signin;
---
+++ app/sign-up/page.tsx
@@ -0,0 +1,7 @@
+export default function signup() {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+    </div>
+  );
+}
